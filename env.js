5c3e4cc5ed41a9788cf5db7a14d9fc1120725771
Marcos Vinícius da Silva <marcosvds2002@gmail.com>
2023-03-06 16:10:10 -0300

    R2-F1-Backend-Autenticacao

---
+++ env.js
@@ -0,0 +1,8 @@
+//AMBIENTE DE PRODUÇÃO
+// export const URL = "giro-paulista.vercel.app"
+
+//AMBIENTE RELEASE
+// export const URL = "giro-paulista-release.vercel.app"
+
+//AMBIENTE DE DESENVOLVIMENTO
+export const URL = "[::1]"
